af1fcefee91533873304f8bc3ca0b54262292b6d
ricardonetmind <ricardoag@expert.netmind.es>
2022-09-23 15:55:26 +0200

    update

---
+++ Resources/Exercises/sol/Iris.sql
@@ -0,0 +1,13 @@
+/****** Object:  Table [dbo].[AzureMetadataTest]    Script Date: 6/18/2018 1:25:50 PM ******/
+DROP TABLE IF EXISTS dbo.[iris]
+GO
+
+CREATE TABLE dbo.iris
+(
+petal_length float,
+petal_width float,
+variety VARCHAR(50)
+)
+GO
+
+
